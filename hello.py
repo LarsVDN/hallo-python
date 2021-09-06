948dc509e2be1864fff7f33c041dd4f75c532181
Lars van den Nieuwendijk <larsvdnieuwendijk@t-mobilethuis.nl>
2021-09-06 10:21:03 +0200

    F1.2.01.L3

---
+++ hello.py
@@ -0,0 +1,12 @@
+print("Hello wereld van...")
+print("  ")
+print("    pppp    y     y   ttttttt   h   h    oooo   nnnn       ")
+print("    p   p    y   y       t      h   h   o    o  n   n")
+print("    p   p     y y        t      h   h   o    o  n   n")
+print("    pppp       y         t      hhhhh   o    o  n   n")
+print("    p          y         t      h   h   o    o  n   n")
+print("    p          y         t      h   h   o    o  n   n")
+print("    p          y         t      h   h    oooo   n   n")
+
+# Lars van den Nieuwendijk
+# 99048376
